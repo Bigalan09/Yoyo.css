51f80f88cc98ff617d783ed6a435974a522ff72a
Alan Gardner <alan.gardner@Moorhouse.int>
2016-05-26 11:54:41 +0100

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var rename = require("gulp-rename");
+
+gulp.task('styles', function() {
+    gulp.src('./*.sass')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'))
+});
+
+gulp.task('minify-css', ['styles'], function() {
+    return gulp.src('css/*.css')
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css'));
+});
+
+//Watch task
+gulp.task('default', function() {
+    gulp.watch('sass/**/*.scss', ['styles', 'minify-css']);
+});
