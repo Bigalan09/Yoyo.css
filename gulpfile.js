a33cd3dd2be0f43dcb435cf4b8cd1d6072a87c76
Bigalan09 <bigalan09@gmail.com>
2016-06-01 12:12:41 +0100

    Added browser sync support to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 var rename = require("gulp-rename");
+var browserSync = require('browser-sync').create();
 
 gulp.task('styles', function() {
     gulp.src('./*.sass')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'))
+        .pipe(gulp.dest('./documentation/css/'))
+        .pipe(browserSync.stream());
 });
 
 gulp.task('minify-css', ['styles'], function() {
@@ -22,7 +25,18 @@ gulp.task('minify-css', ['styles'], function() {
 
 gulp.task('build', ['styles', 'minify-css']);
 
+// Static server
+gulp.task('develop', ['default'], function() {
+    browserSync.init({
+        files: "../css/yoyo.css",
+        server: {
+            baseDir: "./documentation/"
+        }
+    });
+    gulp.watch("documentation/*.html").on('change', browserSync.reload);
+});
+
 //Watch task
-gulp.task('default', function() {
+gulp.task('default', ['build'], function() {
     gulp.watch('sass/**/*.sass', ['styles', 'minify-css']);
 });
